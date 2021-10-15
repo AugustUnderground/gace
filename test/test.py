59c338ef143749666941fa76dd1125ea16526d08
Yannick Uhlmann <augustunderground@protonmail.com>
2021-10-15 17:25:01 +0200

    python test adjusted

--- test/test.py
+++ test/test.py
@@ -1,13 +1,18 @@
+import os
 import gym
 from stable_baselines3.common.env_checker import check_env
 
-env = gym.make( "gym_ad:symmetrical-amplifier-v0"
-              , nmos_prefix = "../models/90nm-nmos"
-              , pmos_prefix = "../models/90nm-pmos"
-              , lib_path    = "../libs/90nm_bulk.lib"
+HOME = os.path.expanduser('~')
+
+env = gym.make( f'gym_ad:sym-amp-xh035-v0'
+              , nmos_path     = f'../models/xh035-nmos'
+              , pmos_path     = f'../models/xh035-pmos'
+              , ckt_path      = f'{HOME}/Workspace/ACE/ace/resource/xh035-3V3/op2'
+              , pdk_path      = f'/mnt/data/pdk/XKIT/xh035/cadence/v6_6/spectre/v6_6_2/mos'
+              , random_target = False
               , )
 
 check_env( env
          , warn              = True
-         , skip_render_check = False
+         , skip_render_check = True
          , )
