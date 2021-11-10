aaceb964f64cfeed7bf1e0db787d6f151980a5e6
Yannick Uhlmann <augustunderground@protonmail.com>
2021-11-10 15:02:16 +0100

    mwe

--- examples/mwe.py
+++ examples/mwe.py
@@ -1,18 +1,15 @@
 import os
 import gym
-from stable_baselines3.common.env_checker import check_env
+from gace import check_env
 
 HOME = os.path.expanduser('~')
 
-env = gym.make( f'gym_ad:sym-amp-xh035-v0'
-              , nmos_path     = f'../models/xh035-nmos'
-              , pmos_path     = f'../models/xh035-pmos'
-              , ckt_path      = f'{HOME}/Workspace/ACE/ace/resource/xh035-3V3/op2'
+op = 'op2'
+
+env = gym.make( f'gace:{op}-xh035-v1'
+              , ckt_path      = f'{HOME}/Workspace/ACE/ace/resource/xh035-3V3/{op}'
               , pdk_path      = f'/mnt/data/pdk/XKIT/xh035/cadence/v6_6/spectre/v6_6_2/mos'
-              , random_target = False
+              , random_target = True
               , )
 
-check_env( env
-         , warn              = True
-         , skip_render_check = True
-         , )
+check_env(env)
