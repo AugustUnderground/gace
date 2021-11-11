077ae4ce70bebac43c800a8ad58a431c2c968c55
Yannick Uhlmann <augustunderground@protonmail.com>
2021-11-11 18:35:24 +0100

    no more paths

--- examples/mwe.py
+++ examples/mwe.py
@@ -4,9 +4,9 @@ from gace import check_env
 
 HOME = os.path.expanduser('~')
 
-op = 'op2'
+op = 'op1'
 
-env = gym.make( f'gace:{op}-xh035-v1'
+env = gym.make( f'gace:{op}-xh035-v1')
               , ckt_path      = f'{HOME}/Workspace/ACE/ace/resource/xh035-3V3/{op}'
               , pdk_path      = f'/mnt/data/pdk/XKIT/xh035/cadence/v6_6/spectre/v6_6_2/mos'
               , random_target = True
