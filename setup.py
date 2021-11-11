9a93b62ee73f1ac0bb24c7b5aa38ebe9e1946fc3
Yannick Uhlmann <augustunderground@protonmail.com>
2021-11-11 18:35:08 +0100

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as req:
     requirements = req.read().splitlines()
  
 setuptools.setup( name                          = package_name
-                , version                       = '0.1.0'
+                , version                       = '0.2.0'
                 , author                        = 'Yannick Uhlmann'
                 , author_email                  = 'augustunderground@protonmail.com'
                 , description                   = 'Analog circuit design gym environments for operational amplifiers'
